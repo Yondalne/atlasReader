aa5e68c0a0515961215ed6f10a05498d432c16c9
Yond <benalicherif.yd@gmail.com>
2023-01-01 12:11:58 +0000

    detail page second prototype design

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from "./router"
 import Unicon from "vue-unicons"
-import { uniApps, uniBookmark, uniFavorite, uniSearch, uniClockEight, uniUser, uniSignout, uniLayerGroup } from 'vue-unicons/dist/icons' 
+import { uniApps, uniBookmark, uniFavorite, uniSearch, uniClockEight, uniUser, uniSignout, uniLayerGroup, uniEye } from 'vue-unicons/dist/icons' 
 
-Unicon.add([uniApps, uniBookmark, uniClockEight, uniFavorite, uniSearch, uniUser, uniSignout, uniLayerGroup])
+Unicon.add([uniApps, uniBookmark, uniClockEight, uniFavorite, uniSearch, uniUser, uniSignout, uniLayerGroup, uniEye])
 
 
 const app = createApp(App) 
--- src/pages/user/DetailPage.vue
+++ src/pages/user/DetailPage.vue
@@ -1,23 +1,104 @@
 <template>
-    <div class="">
-        <div class="w-[99vw] h-full absolute top-0 right-0 z-[-1] overflow-hidden">
-            <video src="/testbg.mp4" :autoplay="true" :controls="false" :muted="true"></video>
-        </div>
-        <div class="flex flex-row grow-1 mx-auto px-16 bg-gradient-to-b from-white/80 to-white absolute bottom-0 border-none rounded-t-xl">
-            <div class="flex flex-row mt-16">
-                <div class="w-1/4 pr-16">
-                    <img src="/kny.jpg" alt="">
-                </div>
-                <div>
-                    <h2 class="font-bold text-lg text-blue-900">Solo leveling</h2>
-                    <p>Synopsis</p>
-                    <div>
-
+    <div class="h-auto relative">
+        <a href="#detail">
+            <video class="object-cover sticky top-[5rem] left-[0] translate-x-[-0%] translate-y-[-0%] z-[-1]" src="/testbg.mp4" :autoplay="true" :controls="false" :muted="true"></video>
+        </a>
+        <div class="bg-gradient-to-b from-white/60 to-white rounded-t-full">
+            <div id="detail" class="h-40"></div>
+            <div class="h-full flex flex-row grow-1 mx-auto px-16 border-none rounded-t-xl gap-32 ">
+                <div class="w-2/3 flex flex-row bg-white gap-16 pr-16">
+                    <div class="w-2/5">
+                        <img src="/kny.jpg" alt="">
+                    </div>
+                    <div class="w-[60%]">
+                        <h2 class="font-bold text-4xl text-blue-900 mt-8">Kimetsu no Yaiba</h2>
+                        <div class="bg-[#faf8f7] mt-6 p-5 rounded-lg drop-shadow-lg">
+                            <h3 class="font-bold text-xl mb-5 mx-5 underline">Synopsis</h3>
+                            <p class="text-lg leading-loose text-justify">
+                                L'histoire se situe au Japon pendant l'ère Taisho (1912-1926). 
+                                Kamado Tanjirou est un jeune vendeur de charbon qui vivait dans les montagnes avec sa famille. 
+                                Sa vie paisible a été bouleversée quand sa famille a été massacré par un féroce oni, un démon issu du folklore japonais. 
+                                Les seuls survivants sont lui et sa soeur Nezuko, transformée en oni par son meurtrier. 
+                                Pour guérir sa soeur et venger sa famille, ils doivent faire un long voyage. Une aventure épique remplie de combats sanglants débute !
+                            </p>
+                        </div>
+                        <div class="mt-16">
+                            <h3 class="font-bold text-xl my-5 mx-5 underline">Chapters</h3>
+                            <ul class="text-lg list-disc">
+                                <div class="py-5 px-10 bg-[#faf8f7] drop-shadow-lg rounded-lg mb-5">
+                                    <li>
+                                         <div class="font-medium flex flex-row justify-between items-center">
+                                            <p class="box">Chapter ?  : <span class="ml-8">Title</span></p>
+                                            <div class="flex items-center flex-row gap-x-5">
+                                                <unicon name="eye"></unicon>
+                                                <unicon name="favorite"></unicon>
+                                            </div> 
+                                        </div>
+                                    </li>
+                                </div>
+                                <div class="py-5 px-10 bg-[#faf8f7] drop-shadow-lg rounded-lg mb-5">
+                                    <li>
+                                         <div class="font-medium flex flex-row justify-between items-center">
+                                            <p class="box">Chapter ?  : <span class="ml-8">Title</span></p>
+                                            <div class="flex items-center flex-row gap-x-5">
+                                                <unicon name="eye"></unicon>
+                                                <unicon name="favorite"></unicon>
+                                            </div> 
+                                        </div>
+                                    </li>
+                                </div>
+                                <div class="py-5 px-10 bg-[#faf8f7] drop-shadow-lg rounded-lg mb-5">
+                                    <li>
+                                         <div class="font-medium flex flex-row justify-between items-center">
+                                            <p class="box">Chapter ?  : <span class="ml-8">Title</span></p>
+                                            <div class="flex items-center flex-row gap-x-5">
+                                                <unicon name="eye"></unicon>
+                                                <unicon name="favorite"></unicon>
+                                            </div> 
+                                        </div>
+                                    </li>
+                                </div>
+                                <div class="py-5 px-10 bg-[#faf8f7] drop-shadow-lg rounded-lg mb-5">
+                                    <li>
+                                         <div class="font-medium flex flex-row justify-between items-center">
+                                            <p class="box">Chapter ? : <span class="ml-8">Title</span></p>
+                                            <div class="flex items-center flex-row gap-x-5">
+                                                <unicon name="eye"></unicon>
+                                                <unicon name="favorite"></unicon>
+                                            </div> 
+                                        </div>
+                                    </li>
+                                </div>
+                                <div class="py-5 px-10 bg-[#faf8f7] drop-shadow-lg rounded-lg mb-5">
+                                    <li>
+                                         <div class="font-medium flex flex-row justify-between items-center">
+                                            <p class="box">Chapter ? : <span class="ml-8">Title</span></p>
+                                            <div class="flex items-center flex-row gap-x-5">
+                                                <unicon name="eye"></unicon>
+                                                <unicon name="favorite"></unicon>
+                                            </div> 
+                                        </div>
+                                    </li>
+                                </div>
+                                <div class="py-5 px-10 bg-[#faf8f7] drop-shadow-lg rounded-lg mb-5">
+                                    <li>
+                                         <div class="font-medium flex flex-row justify-between items-center">
+                                            <p class="box">Chapter ? : <span class="ml-8">Title</span></p>
+                                            <div class="flex items-center flex-row gap-x-5">
+                                                <unicon name="eye"></unicon>
+                                                <unicon name="favorite"></unicon>
+                                            </div> 
+                                        </div>
+                                    </li>
+                                </div>
+                                
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="mt-16">
-                <ranks></ranks>
+                <div class="flex flex-row grow h-auto bg-white pt-8">
+                    <ranks></ranks>
+                </div>
             </div>
         </div>
     </div>
