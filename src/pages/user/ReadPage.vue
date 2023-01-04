fb0edf34dac7428e73e45a274ab759988b71b86b
Yond <benalicherif.yd@gmail.com>
2023-01-04 07:47:08 +0000

    init reading page

---
+++ src/pages/user/ReadPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-xl">Let's read</h1>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "../pages/user/HomePage.vue";
 import DetailPage from "../pages/user/DetailPage.vue";
+import ReadPage from "../pages/user/ReadPage.vue";
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         name: "Detail",
         path: "/detail",
         component: DetailPage,
+    },
+    {
+        name: "Read",
+        path: "/read",
+        component: ReadPage,
     },
 ];
 
